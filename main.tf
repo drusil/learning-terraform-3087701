e480667a28e37db10b96a2118c3ad1e0d2305b79
drusil <czajkajj@gmail.com>
2023-05-21 17:50:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,9 +29,10 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_sg {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
+  name    = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
   
