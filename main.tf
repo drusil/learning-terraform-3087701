52cf4cd6917b012edc41bd3541d7bd7c24ee565e
drusil <czajkajj@gmail.com>
2023-05-21 15:56:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,28 +40,28 @@ resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
-  protocole   = "tcp"
+  protocol   = "tcp"
   cidr_blocks = ["0.0.0.0/0"] 
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
-  protocole   = "tcp"
+  protocol   = "tcp"
   cidr_blocks = ["0.0.0.0/0"] 
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_http_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocole   = "-1"
+  protocol   = "-1"
   cidr_blocks = ["0.0.0.0/0"] 
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
